d2d3bd8688b4031903378125a8a11a693561115b
Zachary Fallon <zpfallon@Zacharys-MacBook-Pro-2.local>
2016-04-16 15:21:47 -0400

    Just run starfile.py to remake the csv files.

--- ddl/star_scrape/starfile.py
+++ ddl/star_scrape/starfile.py
@@ -2,24 +2,21 @@ from bs4 import BeautifulSoup
 soup = BeautifulSoup(open("StarNames.html"))
 
 constellations = open("con.csv","w")
-# starstrans = open("star_trans.csv","w")
+starstrans = open("star_trans.csv","w")
 
 
-#This csv file is complete so dont run this for loop anymore.
+#Constellation abbreviations and translations
 for x in soup.find_all('tr'):
     for y in x.find_all('b'):
-        # print(y.text.split("(")[1].split(",")[0]+","+y.text.split(":")[1])
         translation = y.text.split(":")[1]
         constellations.write(y.text.split("(")[1].split(",")[0]+","+translation.replace(',',' ').replace('\n',' ')+"\n")
 
-#This gets starnames and translations correctly. Some weird formatting stuff happening though.
-# for x in soup.find_all('tr'):
-#     for y in x.find_all('td'):
-#         for z in y.find_all('i'):
-#             translation = str(z.parent.next_sibling.next_sibling.next_sibling.next_sibling.next_sibling.next_sibling.text)
-#             # print (z.text+",\""+translation.replace('\n',' ')+"\"\n")
-#             starstrans.write(z.text.replace(',',' ')+","+translation.replace('\n',' ').replace(',',' ')+"\n")
+#Star names and translations
+for x in soup.find_all('tr'):
+    for y in x.find_all('td'):
+        for z in y.find_all('i'):
+            translation = str(z.parent.next_sibling.next_sibling.next_sibling.next_sibling.next_sibling.next_sibling.text)
+            starstrans.write(z.text.replace(',',' ')+","+translation.replace('\n',' ').replace(',',' ')+"\n")
 
-# starstrans.close()
+starstrans.close()
 constellations.close()
-#test comment
