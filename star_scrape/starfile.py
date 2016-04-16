8e27bd4b0cbc9c6569fd74da8c7c0f7097667eb8
Zachary Fallon <zpfallon@Zacharys-MacBook-Pro-2.local>
2016-04-16 12:46:44 -0400

    Starfile now gets the neccessary info for constellations. Abreviations and translations.

--- star_scrape/starfile.py
+++ star_scrape/starfile.py
@@ -4,8 +4,8 @@ soup = BeautifulSoup(open("StarNames.html"))
 #b gets you the constellation text
 for x in soup.find_all('tr'):
     for y in x.find_all('b'):
-        print(y.text)
+        print(y.text.split("(")[1].split(",")[0]+","+y.text.split(":")[1])
 
-for x in soup.find_all('tr'):
-    for y in x.find_all('td'):
-        print(y.text)
+# for x in soup.find_all('tr'):
+#     for y in x.find_all('td'):
+#         print(y.text)
