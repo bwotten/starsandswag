3fafb1285bdb42e31fe95cfbfd25e089c07aad9f
bwotten <bwotten@email.wme.edu>
2016-04-25 21:10:12 -0400

    Rookie mistake

--- gui.py
+++ gui.py
@@ -204,8 +204,8 @@ class application(Tk):
 			self.canvas.tag_bind(star_id, "<Enter>", self.enter)
 			self.canvas.tag_bind(star_id, "<Leave>", self.leave)
 
-		self.canvas.draw_rectangle(0, 0, self.screen_width * .05, self.screen_height, fill = "yellow")
-		self.canvas.draw_rectangle(self.screen_width * .95, 0, self.screen_width , self.screen_height, fill = "yellow")
+		self.canvas.create_rectangle(0, 0, self.screen_width * .05, self.screen_height, fill = "yellow")
+		self.canvas.create_rectangle(self.screen_width * .95, 0, self.screen_width , self.screen_height, fill = "yellow")
 
 	def getX(self, star_az):
 		#fuckton of cool trig here
